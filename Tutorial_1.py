12829becd31686bf2ecb3d3ecf96fea6112a34c0
garciadiego <diegogarcia@MacBookPro.local>
2016-09-16 16:48:57 -0400

    First tensor lines

---
+++ Tutorial_1.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Import data
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow as tf
