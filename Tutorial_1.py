eccc4ea0ff65cf01432255cd6d565e90678fb86f
garciadiego <diegogarcia@MacBookPro.local>
2016-09-17 15:38:18 -0400

    Tutorial 1
    Final Tutorial 1 from google tensor flow

--- Tutorial_1.py
+++ Tutorial_1.py
@@ -1,3 +1,5 @@
+#tutorial number 1
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -6,3 +8,36 @@ from __future__ import print_function
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', '/Users/diegogarcia/Desktop/Deep_Learning/Tutorial_1/temp', 'Directory for storing data')
+
+mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+sess = tf.InteractiveSession()
+
+# Create the model
+x = tf.placeholder(tf.float32, [None, 784])  #image size 28x28=784
+W = tf.Variable(tf.zeros([784, 10])) 
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b) #matrix multplication
+
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.55).minimize(cross_entropy)
+
+# Train
+tf.initialize_all_variables().run()
+for i in range(1000): #number of interations
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  train_step.run({x: batch_xs, y_: batch_ys})
+
+# Test trained model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
+
+summary_writer = tf.train.SummaryWriter('/Users/diegogarcia/Desktop/Deep_Learning/Tutorial_1', sess.graph_def)
+
